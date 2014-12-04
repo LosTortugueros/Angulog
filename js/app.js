94e8238753e9c1d0b23641b3bb48523c4ea8ec13
thetlk <jeremieboutoille@gmail.com>
2014-12-04 22:38:25 +0100

    Working tab

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 
 (function(){
 
-    var app = angular.module("angulog", []);
+    var app = angular.module("angulog", ['ui.bootstrap']);
+
+    app.controller("ContainerController", function(){
+        this.tab = 1;
+
+        this.setTab = function(id)
+        {
+            this.tab = id;
+        }
+
+        this.isTab = function(id)
+        {
+            return this.tab == id;
+        }
+
+    });
 
 })();
