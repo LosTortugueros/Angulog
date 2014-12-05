40b1788e50b562ec9bdbf438a5a76cea7a073e1d
thetlk <jeremieboutoille@gmail.com>
2014-12-05 03:48:33 +0100

    Premiers diagrammes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 (function(){
 
-    var app = angular.module("angulog", ['ui.bootstrap']);
+    var app = angular.module("angulog", ['ui.bootstrap', 'angularChart']);
 
     app.controller("ContainerController", function(){
         this.tab = 1;
@@ -17,5 +17,88 @@
         }
 
     });
+
+    app.controller("PCController", ["$scope", "$http", function($scope, $http){
+
+        $scope.datasetClicks = [];
+
+        $http.get('http://0.0.0.0:3000/david/click').success(function(data, status, headers, config){
+
+            $scope.datasetClicks = data;
+
+        });
+
+        $scope.schemaClicks = {
+            pseudo: {
+                type: 'string',
+                name: 'Personne'
+            }
+        };
+        $scope.optionsClicks = {
+            rows: [{
+                key: 'click',
+                type: 'bar',
+                name: 'Nombre de clicks',
+                color: '#42a5f5'
+            }],
+            xAxis: {
+                key: 'pseudo'
+            }
+        };
+
+        $scope.datasetKeys = [];
+
+        $http.get('http://0.0.0.0:3000/david/click').success(function(data, status, headers, config){
+
+            $scope.datasetKeys = data;
+
+        });
+
+        $scope.schemaKeys = {
+            pseudo: {
+                type: 'string',
+                name: 'Personne'
+            }
+        };
+        $scope.optionsKeys = {
+            rows: [{
+                key: 'click',
+                type: 'bar',
+                name: 'Nombre de clicks',
+                color: '#42a5f5'
+            }],
+            xAxis: {
+                key: 'pseudo'
+            }
+        };
+
+        /*$scope.data = [
+            {x: 0, value: 4, otherValue: 14},
+            {x: 1, value: 8, otherValue: 1},
+            {x: 2, value: 15, otherValue: 11},
+            {x: 3, value: 16, otherValue: 147},
+            {x: 4, value: 23, otherValue: 87},
+            {x: 5, value: 42, otherValue: 45}
+        ];
+
+        $scope.options = {
+            axes: {
+                x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', min: 0, max: 10, ticks: 2},
+                y: {type: 'linear', min: 0, max: 1, ticks: 5},
+                y2: {type: 'linear', min: 0, max: 1, ticks: [1, 2, 3, 4]}
+            },
+            series: [
+                {y: 'value', color: 'steelblue', thickness: '2px', type: 'area', striped: true, label: 'Pouet'},
+                {y: 'otherValue', axis: 'y2', color: 'lightsteelblue', visible: false, drawDots: true, dotSize: 2}
+            ],
+            lineMode: 'linear',
+            tension: 0.7,
+            tooltip: {mode: 'scrubber', formatter: function(x, y, series) {return 'pouet';}},
+            drawLegend: true,
+            drawDots: true,
+            columnsHGap: 5
+        }*/
+
+    }]);
 
 })();
